01c8038ed05650834f43066e7aaff98798d72bbf
Pierre <pstoiko@gmail.com>
2018-12-27 15:21:14 -0500

    Added second verification for test_delete_first_contact

--- model/contact.py
+++ model/contact.py
@@ -6,3 +6,9 @@ class Contact:
         self.birthday = birthday
         self.birth_month = birth_month
         self.id = id
+
+    def __repr__(self):
+        return '%s:%s' % (self.id, self.firstname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.firstname == other.firstname
