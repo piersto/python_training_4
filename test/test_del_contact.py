a0a1546753b09c0c775f41b6f56a59f3cabb9d28
Pierre <pstoiko@gmail.com>
2018-12-26 23:35:59 -0500

    Загружаем информацию из тестируемого приложения в виде списков Done for test_delete_first_contact

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -5,5 +5,8 @@ from model.contact import Contact
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
         app.contact.fill_in_contact_form(Contact('Contact to be deleted'))
+    old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
 
