7dfd0be4ec02adf9f9e7ec2c18087a563859ef43
Pierre <pstoiko@gmail.com>
2018-12-26 23:34:29 -0500

    Загружаем информацию из тестируемого приложения в виде списков Done for all test_add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,7 +11,10 @@ def test_add_contact(app):
 
 
 def test_add_empty_contact(app):
+    old_contacts = app.contact.get_contact_list()
     app.contact.open_add_new_contact_page()
     app.contact.fill_in_contact_form(Contact(firstname='', middlename='', lastname=''))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
 
 
