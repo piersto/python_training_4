9dbd5676fe6b14e19a91f2b721e9cf9a3865bcc5
Pierre <pstoiko@gmail.com>
2018-12-28 18:53:53 -0500

    Added second verification for test_modify_contact added  birthday='15', middlename='II' to first test - passed.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,7 +5,7 @@ def test_modify_contact_firstname(app):
     if app.contact.count() == 0:
         app.contact.fill_in_contact_form(Contact('Contact to be modified'))
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname='New first name')
+    contact = Contact(firstname='New first name', birthday='15', middlename='II')
     contact.id = old_contacts[0].id
     app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
